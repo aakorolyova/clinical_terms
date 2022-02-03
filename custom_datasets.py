8e30fc4773cc494483ce85c1a13e42baf3f1bf64
Anna Koroleva <anna@relativeinsight.com>
2022-02-03 17:07:27 +0000

    chore: custom datasets updated

--- custom_datasets.py
+++ custom_datasets.py
@@ -31,3 +31,40 @@ class CustomDataset(Dataset):
         return {
             'input_ids': torch.tensor(ids, dtype=torch.long)
         }
+
+class CustomDatasetTextPairs(Dataset):
+
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.data = dataframe
+        self.term1 = dataframe.Term1
+        self.term2 = dataframe.Term2
+        self.target = self.data.label
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.term1)
+
+    def __getitem__(self, index):
+        term1 = str(self.term1[index])
+        term2 = str(self.term2[index])
+
+        inputs = self.tokenizer.encode_plus(
+            term1,
+            term2,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            padding=True,
+            truncation=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'target': torch.tensor(self.target[index], dtype=torch.float)
+        }
